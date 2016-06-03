15dfd95abf73fc65715b39be8189e131a9975362
kennell <kevin@kennell.de>
2016-06-04 00:06:11 +0200

    bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "curldrop",
-    version = "1.0.0",
+    version = "1.0.1",
     packages = find_packages(),
     author = "Kevin Kennell",
     author_email = "kevin@kennell.de",
